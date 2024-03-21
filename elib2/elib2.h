7876c3e3cfd2dfe044a39d76c130bda2e2cb79a1
andreyPogodin <andreyweather@gmail.com>
2024-03-21 12:09:26 +0300

    commit

---
+++ elib2/elib2.h
@@ -0,0 +1,18 @@
+//
+//  elib2.h
+//  elib2
+//
+//  Created by andrey.pogodin on 21.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for elib2.
+FOUNDATION_EXPORT double elib2VersionNumber;
+
+//! Project version string for elib2.
+FOUNDATION_EXPORT const unsigned char elib2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <elib2/PublicHeader.h>
+
+
